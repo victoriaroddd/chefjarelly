81836982eb6b4115895d2dfcd723ce84071fe6a3
victoriaroddd <35376068+victoriaroddd@users.noreply.github.com>
2021-03-02 21:37:10 -0600

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php $name = $_POST['name'];
+
+$email = $_POST['email'];
+
+$message = $_POST['message'];
+
+$formcontent="From: $name \n Message: $message";
+
+$recipient = "chefjarelly.com";
+
+$subject = "Contact Form";
+
+$mailheader = "From: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
